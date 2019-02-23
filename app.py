6fa092580346a0e43dbe08d546a33170cb5d84e9
Michal Wojcik <micwojci@cisco.com>
2019-02-23 12:49:17 +0100

    Adding Regex

--- app.py
+++ app.py
@@ -2,17 +2,25 @@ from flask import Flask
 from flask import jsonify
 import json
 from all_ebooks import get_all
+from werkzeug.routing import BaseConverter
 
 app = Flask(__name__)
+
+class RegexConverter(BaseConverter):
+    def __init__(self, url_map, *items):
+        super(RegexConverter, self).__init__(url_map)
+        self.regex = items[0]
+
+app.url_map.converters['regex'] = RegexConverter
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
-@app.route("/1")
-def find_book():
+@app.route('/<regex("[abcABC0-9]{1,7}"):u>')
+def find_book(u):
 	all_books = json.load(open("all_books.json"))
-	return jsonify(all_books['1'])
+	return jsonify(all_books["%s" %(u)])
 
 @app.route("/all")
 def all():
